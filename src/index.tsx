775d1e957a84b5d1dd682624c7e90df19c1f10fd
tiavina-mika <tiavinamika@gmail.com>
2023-05-06 12:44:50 +0300

    config parse

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,39 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Parse from 'parse';
+import { getServerUrl } from './utils/utils';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const init = () => {
+  // --------------------------------- //
+  // -------------- ENV -------------- //
+  // --------------------------------- //
+  // get app env by browser url
+  const location = window.location;
+  // LOCAL can also mean "accessed by a remote machine (like a Mac) on the local dev network"
+  const hostName = location.hostname;
+  const LOCAL: boolean = hostName.includes('locahost');
+  const PREPROD: boolean = hostName.includes('preprod');
+  const PROD = !LOCAL && !PREPROD;
+
+  (window as any).LOCAL = LOCAL;
+  (window as any).PREPROD = PREPROD;
+  (window as any).PROD = PROD;
+
+  Parse.initialize(process.env.REACT_APP_PARSE_APP_ID ?? 'blogko');
+  Parse.serverURL = getServerUrl() + '/parse';
+
+  const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+  );
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+init();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/utils/utils.ts
@@ -0,0 +1,19 @@
+/**
+ * get the server url
+ * we need it to connect to the parse server or an server side api (ex: via axios)
+ * NOTE: this function should be called after defining the window.LOCAL, window.PREPROD, window.PROD first
+ * @returns
+ */
+export const getServerUrl = (): string => {
+  if ((window as any).LOCAL) {
+    const SERVER_PORT = 8082;
+    const location = window.location;
+    return location.protocol + '//' + location.hostname + ':' + SERVER_PORT;
+  }
+
+  if ((window as any).PREPROD) {
+    return 'https://preprod-url.com';
+  }
+
+  return 'https://prod-url.com';
+};
