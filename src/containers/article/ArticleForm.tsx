ddf594e922ccfee3bb56286ce006d1ccb680f145
tiavina-mika <tiavinamika@gmail.com>
2023-05-13 18:01:28 +0300

    add zod validation

--- src/containers/article/ArticleForm.tsx
+++ src/containers/article/ArticleForm.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { Stack, Button, TextField } from '@mui/material';
 import { ChangeEvent, useState } from 'react';
 import { IArticle, IArticleInput } from '../../types/article.type';
+import { articleSchema } from '../../utils/vaildations/article.validations';
 
 type Props = {
   onSave: (values: any) => void;
@@ -24,11 +25,20 @@ const ArticleForm = ({ onSave, article }: Props) => {
     onSave(values)
   }
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setValues((prev) => ({
-      ...prev,
-      [event.target.name]: event.target.value,
-    }))
+  const handleChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    const newValues = {
+      ...values,
+      [event.target.name]: event.target.value
+    };
+  
+    const validation = await articleSchema.parseAsync(newValues)
+
+    if (!(validation as any).success) {
+      console.log('validation: ', validation);
+      return;
+    }
+
+    setValues(newValues)
   };
 
   return (
